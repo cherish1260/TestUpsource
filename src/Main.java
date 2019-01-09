e2c3cab39ab0261a57af6980fb188aca9cd80cb8
史晓霞 <dx910114@126.com>
2019-01-09 14:26:35 +0800

    ADD：增加乘法方法

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,10 @@ public class Main {
         return a - b;
     }
 
+    public static int multi(int a, int b){
+        return a * b;
+    }
+
     public static void main(String[] args) {
         System.out.println(add(1,2));
     }
