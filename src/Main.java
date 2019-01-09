bce090cedcfde06e71217fd47cdf5c3d5d1f5969
史晓霞 <dx910114@126.com>
2019-01-09 14:23:16 +0800

    ADD：增加减法

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
         return a + b;
     }
 
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+
     public static void main(String[] args) {
         System.out.println(add(1,2));
     }
